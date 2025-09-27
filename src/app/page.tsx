0a7b307efbf276f3c86b0ae63a7976e0d14b3ccd
MukitHossen7 <hossenmukit7@gmail.com>
2025-09-27 23:11:58 +0600

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
@@ -98,6 +99,7 @@ export default function Home() {
           Go to nextjs.org →
         </a>
       </footer>
+      <Button>Click me</Button>
     </div>
   );
 }
