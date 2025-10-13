8d8bc85898d57709bdc6b7370330dd4fdde8a7fb
MukitHossen7 <hossenmukit7@gmail.com>
2025-10-13 10:24:23 +0600

    fix: add token retrieval for login and update authorization header

--- src/actions/auth/auth.ts
+++ src/actions/auth/auth.ts
@@ -1,15 +1,21 @@
 "use server";
 
 import { FieldValues } from "react-hook-form";
+import { cookies } from "next/headers";
 
 export const createLogin = async (loginData: FieldValues) => {
+  const cookieStore = await cookies();
+  const token = cookieStore.get("accessToken")?.value;
+  if (!token) throw new Error("No token found");
+
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/auth/login`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
     },
     body: JSON.stringify(loginData),
-    credentials: "include",
+    // credentials: "include",
   });
 
   if (!res.ok) {
